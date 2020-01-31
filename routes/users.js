a6cad660e05d71a8f38240b59bf471cc10179b5b
Arthur Bachelet <arthur.bachelet@orange.fr>
2020-01-31 23:50:59 +0100

    back search-login wo session- give all user- submit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser');
+var multer = require('multer');
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
 
 module.exports = router;
