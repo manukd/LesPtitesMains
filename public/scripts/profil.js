b313b67734bb4b1629068d219b0b34d7beb93044
Arthur Bachelet <arthur.bachelet@orange.fr>
2020-02-01 03:25:30 +0100

    show informations on click in profiles

--- public/scripts/profil.js
+++ public/scripts/profil.js
@@ -1,5 +1,5 @@
 class Profil{
-	
+
 	constructor(lastname, firstname, birth,genre, address, power, nbLifeSaved, nbBadGuys ) {
 		this.lastname = lastname;
 		this.firstname = firstname;
@@ -15,41 +15,41 @@ class Profil{
 	showDetail(){
 		var detail_view = document.createElement('div');
 		detail_view.id = "profil_detail"
-		detail_view.setAttribute('class',' bg-light search-block p-5 w-80 l-10 text-left');
+		detail_view.setAttribute('class',' bg-light text-dark search-block p-5 w-80 l-10 text-left');
 		detail_view.style.bottom = "30%";
 		detail_view.innerHTML = '\
 								<div class="row margin-t">\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Nom : '+ this.lastname +'</label>\
+		                                <label class=" ">Nom : '+ this.lastname +'</label>\
 		                            </div>\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Prénom : '+ this.firstname +'</label>\
-		                            </div>\
-		                        </div>\
-		                        <div class="row margin-t">\
-		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Date de naissance : '+ this.birth +'</label>\
-		                            </div>\
-		                            <div class="col">\
-		                                <label class="text-whitesmoke">Genre : '+ this.genre +'</label>\
+		                                <label class="">Prénom : '+ this.firstname +'</label>\
 		                            </div>\
 		                        </div>\
 		                        <div class="row margin-t">\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Adresse : '+ this.address +'</label>\
+		                                <label class="">Date de naissance : '+ this.birth +'</label>\
+		                            </div>\
+		                            <div class="col">\
+		                                <label class="">Genre : '+ this.genre +'</label>\
 		                            </div>\
 		                        </div>\
 		                        <div class="row margin-t">\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Pouvoir : '+ this.power +'</label>\
+		                                <label class="">Adresse : '+ this.address +'</label>\
 		                            </div>\
 		                        </div>\
 		                        <div class="row margin-t">\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Nombre de vies sauvées : '+ this.nbLifeSaved +'</label>\
+		                                <label class="">Pouvoir : '+ this.power +'</label>\
+		                            </div>\
+		                        </div>\
+		                        <div class="row margin-t">\
+		                            <div class="col">\
+		                                <label class=" ">Nombre de vies sauvées : '+ this.nbLifeSaved +'</label>\
 		                            </div>\
 		                            <div class="col">\
-		                                <label class="text-whitesmoke ">Nombre de criminels arrêtés : '+ this.nbBadGuys +'</label>\
+		                                <label class="">Nombre de criminels arrêtés : '+ this.nbBadGuys +'</label>\
 		                            </div>\
 		                        </div></br>\
 		                        	<button class="row btn btn-danger rounded-25 m-auto" type="button" onclick="closeDetail();">X</button>';
