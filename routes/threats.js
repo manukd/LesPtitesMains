5ca2b69e1b386f90280c7241f6f82091d01c4240
Arthur Bachelet <arthur.bachelet@orange.fr>
2020-02-01 09:51:16 +0100

    gestion des menaces

--- routes/threats.js
+++ routes/threats.js
@@ -2,15 +2,25 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27018/lesptitesmains', {useNewUrlParser: true});
 /* GET users listing. */
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 router.get('/', function(req, res, next) {
     var sess = req.session
     if (sess.username) {
-      res.render('threats', {sess: req.session});
+      db.collection("threats").find({}).toArray(function(err,result) {
+        if (err) throw err;
+        console.log(result);
+        res.render('threats', { threats: result , sess: req.session });
+      })
     }
     else {
-      res.render('error', {message: "Désolé, cette page est seulement accessible aux héros"});
+      res.render('error', {message: "Désolé, cette page est seulement accéssible aux héros"});
     }
 });
 
