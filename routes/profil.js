4d46b934fee1ef63ea5acc6cd44e26bcd56d7443
Arthur Bachelet <arthur.bachelet@orange.fr>
2020-02-01 10:36:12 +0100

    recherche profil

--- routes/profil.js
+++ routes/profil.js
@@ -13,17 +13,36 @@ router.get('/', function(req, res, next) {
   db.collection("users").find({}).toArray(function(err,result) {
     if (err) throw err;
     console.log(result);
-    res.render('profils', { user: result , sess: req.session });
+    res.render('profils', { user: result , sess: req.session, move: 1 });
   })
 });
 
 router.post('/', function (req, res, next) {
-  db.collection("users").find({name: req.body.request}).toArray(function(err,result) {
-    console.log(result);
-    if (err) throw err;
-    console.log(result);
-    res.render('profils', { user: result , sess: req.session });
-  })
+  if (req.body.choiceMode=="1") {
+    db.collection("users").find({name: req.body.request}).toArray(function(err,result) {
+      console.log(result);
+      if (err) throw err;
+      console.log(result);
+      res.render('profils', { user: result , sess: req.session, move: 0 });
+    })
+  }
+  else if (req.body.choiceMode=="2") {
+    db.collection("users").find({surname: req.body.request}).toArray(function(err,result) {
+      console.log(result);
+      if (err) throw err;
+      console.log(result);
+      res.render('profils', { user: result , sess: req.session, move: 0 });
+    })
+  }
+  else {
+    db.collection("users").find({power: req.body.request}).toArray(function(err,result) {
+      console.log(result);
+      if (err) throw err;
+      console.log(result);
+      res.render('profils', { user: result , sess: req.session, move: 0 });
+    })
+  }
+
 });
 
 module.exports = router;
