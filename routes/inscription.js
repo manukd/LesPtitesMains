3b8d12bf0e5c65475db3dca5b9ee65513ef78703
Arthur Bachelet <arthur.bachelet@orange.fr>
2020-02-01 03:57:07 +0100

    subscribe linked to view

--- routes/inscription.js
+++ routes/inscription.js
@@ -2,10 +2,51 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var mongoose = require('mongoose');
 /* GET users listing. */
+mongoose.connect('mongodb://localhost:27018/lesptitesmains', {useNewUrlParser: true});
+/* GET home page. */
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+var user = new mongoose.Schema({
+  username: String,
+  password: String,
+  name: String,
+  surname: String,
+  age: Number,
+  address: String,
+  gender: String,
+  power: String,
+  saved_lives: Number,
+  arrested_baddies: Number
+});
+
+var User = mongoose.model('Users', user);
 
 router.get('/', function(req, res, next) {
   res.render('inscription', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  console.log("la");
+  console.log(req.body.username);
+  var an_user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    name: req.body.name,
+    surname: req.body.surname,
+    age: req.body.age,
+    address: req.body.address,
+    gender: req.body.gender,
+    saved_lives: req.body.saved_lives,
+    arrested_baddies: req.body.arrested_baddies
+  });
+  an_user.save(function (err, an_user) {
+    if (err) return console.error(err);
+    console.log("ici");
+    console.log(an_user);
+  })
+  res.redirect('/connexion');
 });
 
 module.exports = router;
